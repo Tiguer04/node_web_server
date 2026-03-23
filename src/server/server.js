5f7bf128ff44d1db60b3e683917acac542ac3c40
Tiguer04 <trustedx12@gmail.com>
2026-03-23 15:43:50 -0500

    Se realiza cambio de importaciones y exportanciones a de COMMONJS a ES6

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,9 @@
-const express = require('express');
-const path = require('path');
+//const express = require('express');
+import express from 'express';
+//const path = require('path');
+import path from 'path';
 
-const startServer = (options) =>{
+export const startServer = (options) =>{
   const {port, public_path = 'public'} = options
   
   const app = express();
@@ -20,6 +22,7 @@ const startServer = (options) =>{
 
 }
 
+/*
 module.exports ={
   startServer
-}
+}*/
